36f639e882bb8f305b4365c05a5cc4021228892a
Mariusz <gitnaukagit@gmail.com>
2018-04-25 00:12:59 +0200

    lekcja 4

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,8 +10,9 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Witam wszystkich");
+            Console.WriteLine("Witam wszystkich i Ciebie {0}", args[0]);
             Console.ReadLine();
+            Console.WriteLine();
         }
     }
 }
